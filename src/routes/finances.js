7b1253f01ab205b9fdc1abfe7e1d837479d89fa2
Diego Meira <di.meira@hotmail.com>
2024-05-27 17:02:23 -0300

    Get to finances

--- src/routes/finances.js
+++ src/routes/finances.js
@@ -24,10 +24,10 @@ const validateValue = (value) => {
 router.post("/", async (req, res) => {
   try {
     const { email } = req.headers;
-    const { category_id, title, date, value } = req.body;
+    const { category_id, title, date, value, user_id } = req.body;
 
     // Verificar se todos os campos estão presentes
-    if (!email || !category_id || !title || !date || !value) {
+    if (!email || !category_id || !title || !date || !value || !user_id) {
       return res.status(400).json({ error: "All fields are required" });
     }
 
@@ -77,8 +77,8 @@ router.post("/", async (req, res) => {
 
     // Inserir os dados na tabela
     const insertQuery = {
-      text: "INSERT INTO finances (category_id, title, date, value, email) VALUES ($1, $2, $3, $4, $5) RETURNING *",
-      values: [category_id, title, date, value, email],
+      text: "INSERT INTO finances (category_id, title, date, value, email, user_id) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
+      values: [category_id, title, date, value, email, user_id],
     };
     const insertResult = await db.query(insertQuery);
 
@@ -121,12 +121,10 @@ router.delete("/:id", async (req, res) => {
     };
     const financeResult = await db.query(financeQuery);
     if (financeResult.rows.length === 0) {
-      return res
-        .status(404)
-        .json({
-          error:
-            "Finance record not found or you do not have permission to delete this record",
-        });
+      return res.status(404).json({
+        error:
+          "Finance record not found or you do not have permission to delete this record",
+      });
     }
 
     // Excluir o registro financeiro
@@ -136,12 +134,61 @@ router.delete("/:id", async (req, res) => {
     };
     const deleteResult = await db.query(deleteQuery);
 
-    res
-      .status(200)
-      .json({
-        message: "Finance record deleted successfully",
-        deletedRecord: deleteResult.rows[0],
-      });
+    res.status(200).json({
+      message: "Finance record deleted successfully",
+      deletedRecord: deleteResult.rows[0],
+    });
+  } catch (error) {
+    console.error("Error processing the request:", error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+router.get("/", async (req, res) => {
+  try {
+    const { email } = req.headers;
+    const { date } = req.query;
+
+    // Validação dos parâmetros
+    if (!email || !date) {
+      // Remoção da validação de month e year
+      return res.status(400).json({ error: "Email and date are required" });
+    }
+
+    // Validar o formato da data
+    if (!validateDate(date)) {
+      return res
+        .status(400)
+        .json({ error: "Invalid date format. Use YYYY-MM-DD" });
+    }
+
+    // Obter o user_id com base no email
+    const userQuery = {
+      text: "SELECT id FROM users WHERE email = $1",
+      values: [email],
+    };
+    const userResult = await db.query(userQuery);
+    if (userResult.rows.length === 0) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    const user_id = userResult.rows[0].id;
+
+    // Ajustar a consulta SQL para a data específica
+    const financesQuery = {
+      text: `
+        SELECT fin.title, fin.value, fin.date, fin.user_id, fin.category_id, cat.name
+        FROM finances AS fin
+        JOIN categories AS cat
+        ON fin.category_id = cat.id
+        WHERE fin.user_id = $1 AND fin.date = $2 -- Alteração para usar a data específica
+        ORDER BY fin.date ASC;
+      `,
+      values: [user_id, date], // Passar a data como parâmetro
+    };
+
+    const financesResult = await db.query(financesQuery);
+
+    res.status(200).json(financesResult.rows);
   } catch (error) {
     console.error("Error processing the request:", error);
     return res.status(500).json({ error: "Internal server error" });
